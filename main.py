519a5454908baae24a5c07660c0314c3adbc6305
HaynesX <brandon@haynes.bio>
2022-07-18 07:55:41 +0100

    Changed Error Handling

--- main.py
+++ main.py
@@ -35,126 +35,130 @@ session = inverse_perpetual.HTTP(
 
 
 def poll_bybit():
-    try:
-        while True:
-            with open('pnlData/data.json') as json_file:
-                calculatedProfitAndLoss = json.load(json_file)
-                listOfKeys = list(calculatedProfitAndLoss.keys())
+    while True:
+        time.sleep(5)
+        print("Starting up bot.")
+        try:
+            while True:
+                with open('pnlData/data.json') as json_file:
+                    calculatedProfitAndLoss = json.load(json_file)
+                    listOfKeys = list(calculatedProfitAndLoss.keys())
 
-                if len(listOfKeys) == 0:
-                    filterDate = datetime.strptime('Jun 29 2022  6:30AM', '%b %d %Y %I:%M%p')
-                    filterDateTimestamp = datetime.timestamp(filterDate)
-                else:
-                    filterDateTimestamp = calculatedProfitAndLoss[listOfKeys[0]]["Close_Time"]
-                
-                filterDateTimestampEnd = round(datetime.timestamp(datetime.now()))
-                
+                    if len(listOfKeys) == 0:
+                        filterDate = datetime.strptime('Jun 29 2022  6:30AM', '%b %d %Y %I:%M%p')
+                        filterDateTimestamp = datetime.timestamp(filterDate)
+                    else:
+                        filterDateTimestamp = calculatedProfitAndLoss[listOfKeys[0]]["Close_Time"]
+                    
+                    filterDateTimestampEnd = round(datetime.timestamp(datetime.now()))
+                    
 
-            time.sleep(3)
-
-            allResults = []
-            oldResults = []
-            combinedResults = []
-
-            for eachKey in calculatedProfitAndLoss:
-                oldResult = {
-                    "order_id": calculatedProfitAndLoss[eachKey]["orderID"],
-                    "side": calculatedProfitAndLoss[eachKey]["side"],
-                    "avg_entry_price": calculatedProfitAndLoss[eachKey]["avgEntryPrice"],
-                    "avg_exit_price": calculatedProfitAndLoss[eachKey]["avgClosePrice"],
-                    "created_at": calculatedProfitAndLoss[eachKey]["Close_Time"],
-                    "closed_pnl": calculatedProfitAndLoss[eachKey]["P&L (BTC)"],
-                }
-                oldResults.append(oldResult)
-
-            profitAndLossData = ""
-            pageIncrement = 0
-            while profitAndLossData != None:
-                pageIncrement += 1
-
-                profitAndLossData = session.closed_profit_and_loss(symbol="BTCUSD", limit=50, page=pageIncrement, start_time=filterDateTimestamp, end_time=filterDateTimestampEnd)["result"]["data"]
-
-                if profitAndLossData == None:
-                    continue
-
-                for eachResult in profitAndLossData:
-                    if eachResult["order_id"] not in calculatedProfitAndLoss:
-                        allResults.append(eachResult)
-                        
-                
                 time.sleep(3)
-            
-            allResults.reverse()
 
-            for eachResult in oldResults:
-                combinedResults.append(eachResult)
+                allResults = []
+                oldResults = []
+                combinedResults = []
 
-            for eachResult in allResults:
-                combinedResults.append(eachResult)
+                for eachKey in calculatedProfitAndLoss:
+                    oldResult = {
+                        "order_id": calculatedProfitAndLoss[eachKey]["orderID"],
+                        "side": calculatedProfitAndLoss[eachKey]["side"],
+                        "avg_entry_price": calculatedProfitAndLoss[eachKey]["avgEntryPrice"],
+                        "avg_exit_price": calculatedProfitAndLoss[eachKey]["avgClosePrice"],
+                        "created_at": calculatedProfitAndLoss[eachKey]["Close_Time"],
+                        "closed_pnl": calculatedProfitAndLoss[eachKey]["P&L (BTC)"],
+                    }
+                    oldResults.append(oldResult)
 
-            startingBalance = 0.00908992
+                profitAndLossData = ""
+                pageIncrement = 0
+                while profitAndLossData != None:
+                    pageIncrement += 1
 
-            allRows = []
+                    profitAndLossData = session.closed_profit_and_loss(symbol="BTCUSD", limit=50, page=pageIncrement, start_time=filterDateTimestamp, end_time=filterDateTimestampEnd)["result"]["data"]
 
-            cumulativeBalance = startingBalance
-            cumulativeProfitAndLoss = 0
-            for eachResult in combinedResults:
-                orderID = eachResult["order_id"]
-                side = eachResult["side"]
-                avgEntryPrice = eachResult["avg_entry_price"]
-                avgClosePrice = eachResult["avg_exit_price"]
-                closedProfitAndLoss = eachResult["closed_pnl"]
-                closedProfitAndLossPercentage = (closedProfitAndLoss / cumulativeBalance) * 100 #Needs to multiply by 100 in spreadsheet
-                oldBalance = cumulativeBalance
-                cumulativeBalance += closedProfitAndLoss
-                cumulativeProfitAndLoss += closedProfitAndLoss
-                cumulativeProfitAndLossPercentage = (cumulativeProfitAndLoss / startingBalance) * 100 #Needs to multiply by 100 in spreadsheet
-                created_at = datetime.fromtimestamp(eachResult["created_at"])
-                created_at_string = created_at.strftime("%d/%m/%Y, %H:%M:%S")
-                winOrLoss = ""
+                    if profitAndLossData == None:
+                        continue
+
+                    for eachResult in profitAndLossData:
+                        if eachResult["order_id"] not in calculatedProfitAndLoss:
+                            allResults.append(eachResult)
+                            
                     
-                if closedProfitAndLoss < 0:
-                    winOrLoss = "Loss"
-                else:
-                    winOrLoss = "Win"
-                    
-                    
-                if orderID not in calculatedProfitAndLoss:
-                    calculatedProfitAndLoss[orderID] = {"orderID": orderID, "Close_Time": eachResult['created_at'], "side": side, "avgEntryPrice": avgEntryPrice, "avgClosePrice": avgClosePrice, "P&L (BTC)": closedProfitAndLoss, "P&L %": closedProfitAndLossPercentage, "Cumulative P&L (BTC)": cumulativeProfitAndLoss, "Cumulative P&L %": cumulativeProfitAndLossPercentage, "Previous Balance": oldBalance, "New Balance": cumulativeBalance, "Created at": created_at_string, "WinOrLoss": winOrLoss}
-
-                    allRows.append([orderID, created_at_string, side, avgEntryPrice, avgClosePrice, closedProfitAndLoss, closedProfitAndLossPercentage, cumulativeProfitAndLoss, cumulativeProfitAndLossPercentage, oldBalance, cumulativeBalance, winOrLoss, "", "", "", "", "", ""])
-            
-            
-
-            if len(allRows) > 0:
-                print("New P&L Found. Adding to Google Sheet.")
-                sheet.append_rows(allRows, table_range='A1')
-
-                for eachRow in allRows:
-                    
-                    
-
-                    telegramMessage = f"""
-<b>Trade added to P&L Sheet</b>
-                    
-Side: <b>{eachRow[2]}</b>
-Entry: <b>{eachRow[3]}</b>
-Exit: <b>{eachRow[4]}</b>
-P&L: <b>{round(eachRow[6], 2)}%</b>
-
-<b>{eachRow[1]}</b>
-<b><a href="https://docs.google.com/spreadsheets/d/1gNFpHEs0YXCYgrdGnk-5b8DuOLrwBKtDkfxxRK0GgVc">Google Sheet</a></b>
-"""
-
-                    bot.send_message(TELEGRAM_CHAT_ID, telegramMessage, parse_mode="HTML", disable_web_page_preview=True)
-
                     time.sleep(3)
+                
+                allResults.reverse()
 
-                with open("pnlData/data.json", "w") as outfile:
-                    json.dump(calculatedProfitAndLoss, outfile)
+                for eachResult in oldResults:
+                    combinedResults.append(eachResult)
 
-    except:
-        print("ERROR!")
+                for eachResult in allResults:
+                    combinedResults.append(eachResult)
+
+                startingBalance = 0.00908992
+
+                allRows = []
+
+                cumulativeBalance = startingBalance
+                cumulativeProfitAndLoss = 0
+                for eachResult in combinedResults:
+                    orderID = eachResult["order_id"]
+                    side = eachResult["side"]
+                    avgEntryPrice = eachResult["avg_entry_price"]
+                    avgClosePrice = eachResult["avg_exit_price"]
+                    closedProfitAndLoss = eachResult["closed_pnl"]
+                    closedProfitAndLossPercentage = (closedProfitAndLoss / cumulativeBalance) * 100 #Needs to multiply by 100 in spreadsheet
+                    oldBalance = cumulativeBalance
+                    cumulativeBalance += closedProfitAndLoss
+                    cumulativeProfitAndLoss += closedProfitAndLoss
+                    cumulativeProfitAndLossPercentage = (cumulativeProfitAndLoss / startingBalance) * 100 #Needs to multiply by 100 in spreadsheet
+                    created_at = datetime.fromtimestamp(eachResult["created_at"])
+                    created_at_string = created_at.strftime("%d/%m/%Y, %H:%M:%S")
+                    winOrLoss = ""
+                        
+                    if closedProfitAndLoss < 0:
+                        winOrLoss = "Loss"
+                    else:
+                        winOrLoss = "Win"
+                        
+                        
+                    if orderID not in calculatedProfitAndLoss:
+                        calculatedProfitAndLoss[orderID] = {"orderID": orderID, "Close_Time": eachResult['created_at'], "side": side, "avgEntryPrice": avgEntryPrice, "avgClosePrice": avgClosePrice, "P&L (BTC)": closedProfitAndLoss, "P&L %": closedProfitAndLossPercentage, "Cumulative P&L (BTC)": cumulativeProfitAndLoss, "Cumulative P&L %": cumulativeProfitAndLossPercentage, "Previous Balance": oldBalance, "New Balance": cumulativeBalance, "Created at": created_at_string, "WinOrLoss": winOrLoss}
+
+                        allRows.append([orderID, created_at_string, side, avgEntryPrice, avgClosePrice, closedProfitAndLoss, closedProfitAndLossPercentage, cumulativeProfitAndLoss, cumulativeProfitAndLossPercentage, oldBalance, cumulativeBalance, winOrLoss, "", "", "", "", "", ""])
+                
+                
+
+                if len(allRows) > 0:
+                    print("New P&L Found. Adding to Google Sheet.")
+                    sheet.append_rows(allRows, table_range='A1')
+
+                    for eachRow in allRows:
+                        
+                        
+
+                        telegramMessage = f"""
+    <b>Trade added to P&L Sheet</b>
+                        
+    Side: <b>{eachRow[2]}</b>
+    Entry: <b>{eachRow[3]}</b>
+    Exit: <b>{eachRow[4]}</b>
+    P&L: <b>{round(eachRow[6], 2)}%</b>
+
+    <b>{eachRow[1]}</b>
+    <b><a href="https://docs.google.com/spreadsheets/d/1gNFpHEs0YXCYgrdGnk-5b8DuOLrwBKtDkfxxRK0GgVc">Google Sheet</a></b>
+    """
+
+                        bot.send_message(TELEGRAM_CHAT_ID, telegramMessage, parse_mode="HTML", disable_web_page_preview=True)
+
+                        time.sleep(3)
+
+                    with open("pnlData/data.json", "w") as outfile:
+                        json.dump(calculatedProfitAndLoss, outfile)
+
+        except Exception as e:
+            print("ERROR!")
+            print(e)
 
 
 
